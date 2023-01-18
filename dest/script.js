fda8bbdae39c83fbed715533858cbd346f0717bf
OWarway <oliverwarway@gmail.com>
2023-01-18 20:54:42 +0000

    key event listeners aka movement

--- dest/script.js
+++ dest/script.js
@@ -18,6 +18,7 @@ class Sprite {
     }
     update() {
         this.draw();
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
         // Bottom of the rectangle (this.position.y + this.height)
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -40,6 +41,14 @@ const enemy = new Sprite({
     height: 150,
     width: 50,
 });
+const keys = {
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
+};
 // Creates infinate loop
 const animate = () => {
     window.requestAnimationFrame(animate);
@@ -47,5 +56,38 @@ const animate = () => {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
     enemy.update();
+    player.velocity.x = 0;
+    if (keys.a.pressed) {
+        player.velocity.x = -1;
+    }
+    else if (keys.d.pressed) {
+        player.velocity.x = 1;
+    }
 };
 animate();
+window.addEventListener("keydown", (e) => {
+    const { key } = e;
+    switch (key) {
+        case "d": {
+            keys.d.pressed = true;
+            break;
+        }
+        case "a": {
+            keys.a.pressed = true;
+            break;
+        }
+    }
+});
+window.addEventListener("keyup", (e) => {
+    const { key } = e;
+    switch (key) {
+        case "d": {
+            keys.d.pressed = false;
+            break;
+        }
+        case "a": {
+            keys.d.pressed = false;
+            break;
+        }
+    }
+});
--- src/script.ts
+++ src/script.ts
@@ -47,6 +47,8 @@ class Sprite {
 
   update() {
     this.draw();
+
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     // Bottom of the rectangle (this.position.y + this.height)
@@ -72,6 +74,15 @@ const enemy = new Sprite({
   width: 50,
 });
 
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
 // Creates infinate loop
 const animate = () => {
   window.requestAnimationFrame(animate);
@@ -79,6 +90,42 @@ const animate = () => {
   ctx!.fillRect(0, 0, canvas!.width, canvas!.height);
   player.update();
   enemy.update();
+
+  player.velocity.x = 0;
+
+  if (keys.a.pressed) {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed) {
+    player.velocity.x = 1;
+  }
 };
 
 animate();
+
+window.addEventListener("keydown", (e) => {
+  const { key } = e;
+  switch (key) {
+    case "d": {
+      keys.d.pressed = true;
+      break;
+    }
+    case "a": {
+      keys.a.pressed = true;
+      break;
+    }
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  const { key } = e;
+  switch (key) {
+    case "d": {
+      keys.d.pressed = false;
+      break;
+    }
+    case "a": {
+      keys.d.pressed = false;
+      break;
+    }
+  }
+});
